65952b931d9b5133b8882598e486f203422d68f1
namhosung <71036396+namhosung6023@users.noreply.github.com>
2021-02-04 18:08:51 +0900

    다이어리 바뀐 ui 작업은 완료 기능 추가하는 작업중

--- lib/views/premium/widgets/widget_member_diary.dart
+++ lib/views/premium/widgets/widget_member_diary.dart
@@ -171,7 +171,8 @@ class _MemberDiaryState extends State<MemberDiary> {
     return Column(
       children: [
         const SizedBox(height: 24.0),
-        _buildSubtitle('공복 사진', '일어나시고 배변 후 몸 사진을 찍어 올려주세요.'),
+        _buildSubtitle('공복 사진', 0 , morningBody),
+        _buildSubtitle1('일어나시고 배변 후 몸 사진을 찍어 올려주세요'),
         const SizedBox(height: 24.0),
         _buildWeight(),
         const SizedBox(height: 16.0),
@@ -186,7 +187,8 @@ class _MemberDiaryState extends State<MemberDiary> {
     return Column(
       children: [
         const SizedBox(height: 24.0),
-        _buildSubtitle('자기전 사진', '잠들기 직전 몸 사진을 찍어주세요.'),
+        _buildSubtitle('자기전 사진', 1, nightBody),
+        _buildSubtitle1('일어나시고 배변 후 몸 사진을 찍어 올려주세요'),
         const SizedBox(height: 24.0),
         _buildWeight(),
         const SizedBox(height: 16.0),
@@ -203,7 +205,7 @@ class _MemberDiaryState extends State<MemberDiary> {
         value: value,
         child: Row(
           children: [
-            Text('$text',
+            Text('사진업로드',
                 style: TextStyle(
                     color: Colors.grey,
                     fontSize: 13.0,
@@ -217,30 +219,13 @@ class _MemberDiaryState extends State<MemberDiary> {
     return Column(
       children: [
         _buildTitle('식단 관리'),
-        const SizedBox(height: 16.0),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            height: 32.0,
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(6.0)),
-            child: DropdownButton(
-              value: this._value,
-              underline: Container(),
-              iconSize: 16.0,
-              onChanged: (int value) => setState(() => this._value = value),
-              items: [
-                dropdownItem(0, '아침'),
-                dropdownItem(1, '점심'),
-                dropdownItem(2, '저녁'),
-                dropdownItem(3, '간식'),
-              ],
-            ),
-          ),
-        ),
+        const SizedBox(height: 40),
+        _buildSubtitle3('사진업로드'),
         const SizedBox(height: 8.0),
+        _buildFoodbutton(morningFood, afternoonFood, nightFood, snack, 2,3,4,5),
+        const SizedBox(height: 10.0),
+        _buildUploadMorningFoodSubtitle(),
+        const SizedBox(height: 10.0),
         if (_value == 0)
           _buildUploadImages(morningFood, 2)
         else if (_value == 1)
@@ -271,9 +256,165 @@ class _MemberDiaryState extends State<MemberDiary> {
             ),
           ],
         ),
+        const SizedBox(height: 20.0),
+        _buildUploadAfternoonFoodSubtitle(),
+        const SizedBox(height: 16.0),
+        _buildUploadAfteroonImages(),
+        const SizedBox(height: 16.0),
+        Row(
+          children: [
+            Image.asset('assets/ic_diet.png', width: 24.0, height: 24.0),
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: TextField(
+                style: TextStyle(
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+                decoration: InputDecoration(
+                  isDense: true, // Added this
+                  contentPadding: EdgeInsets.all(8.0),
+                  hintText: '음식이름을 입력해주세요',
+                  hintStyle: TextStyle(color: Color(0xffCCCCCC)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6.0)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
+
+  ///식단관리 사진 업로드 버튼
+  Widget _buildFoodbutton(String imageUrl2,String imageUrl3,String imageUrl4,String imageUrl5, int pictureNumber2, int pictureNumber3, int pictureNumber4, int pictureNumber5) {
+    return Row(
+      children: [
+        Expanded(
+          child: OutlineButton(onPressed: (){
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                backgroundColor: Colors.blue,
+                context: context,
+                builder: ((builder) => bottomSheet()));
+            _pictureNumber = pictureNumber2;
+            imageUrl2 = morningFood;
+          },
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Colors.black12,
+            padding: EdgeInsets.all(0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.1),
+            ),
+            child:
+            Text('아침',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black38
+              ),),
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: OutlineButton(onPressed: (){
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                backgroundColor: Colors.blue,
+                context: context,
+                builder: ((builder) => bottomSheet()));
+            _pictureNumber = pictureNumber3;
+            imageUrl3 = afternoonFood;
+          },
+            color: Colors.black38,
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.1),
+            ),
+            child: Text('점심',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black38
+              ),),
+            ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: OutlineButton(onPressed: (){
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                backgroundColor: Colors.blue,
+                context: context,
+                builder: ((builder) => bottomSheet()));
+            _pictureNumber = pictureNumber4;
+            imageUrl4 = nightFood;
+          },
+            color: Colors.black38,
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.1),
+            ),
+            child: Text('저녁',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black38
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: OutlineButton(onPressed: (){
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                backgroundColor: Colors.blue,
+                context: context,
+                builder: ((builder) => bottomSheet()));
+            _pictureNumber = pictureNumber5;
+            imageUrl5 = snack;
+          },
+            color: Colors.black12,
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.1),
+            ),
+            child: Text('간식',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Colors.black38
+              ),),
+            ),
+        ),
+        const SizedBox(width: 10.0),
+      ],
+    );
+  }
+
+  ///식단관리 서브 타이틀
+  Widget _buildSubtitle3(String value) {
+    return Row(
+      children: [
+        Text(
+          value,
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(width: 150.1),
+      ],
+    );
+  }
+
 
   /// 컨텐츠 헤더 빌드.
   Widget _buildTitle(String value) {
@@ -293,22 +434,133 @@ class _MemberDiaryState extends State<MemberDiary> {
     );
   }
 
-  /// 서브타이틀 빌드.
-  Widget _buildSubtitle(String value, String message) {
+  /// 체중 관리 이미지 업로드
+  Widget _buildSubtitle(String value, int pictureNumber, String imageUrl) {
     return Row(
       children: [
         Text(
           value,
           style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
         ),
-        const SizedBox(width: 16.0),
-        Expanded(
-          child: Text(
-            message,
-            style: TextStyle(color: Color(0xFFFF9B70), fontSize: 12.0),
+        const SizedBox(width: 130.1),
+        FlatButton(onPressed: (){
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              ),
+              backgroundColor: Colors.blue,
+              context: context,
+              builder: ((builder) => bottomSheet()));
+          _pictureNumber = pictureNumber;
+        },
+          padding: EdgeInsets.fromLTRB(34, 0, 34, 0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(9.1),
           ),
-        ),
+          child: Text('파일 업로드',
+          style: TextStyle(
+            fontSize: 11,
+              fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),),
+        color: Colors.black12,),
+        const SizedBox(width: 16.0),
       ],
+    );
+  }
+
+  ///서브 타이틀 밑에 글씨
+  Widget _buildSubtitle1(String value) {
+    return Row(
+      children: [
+        Text(
+            value,
+          style: TextStyle(color: Color(0xFFFF9B70), fontSize: 12.0),
+        )
+      ],
+    );
+  }
+
+  ///식단관리에서 사진업로드시 생기는 타이틀
+  Widget _buildUploadMorningFoodSubtitle() {
+    return Row(
+      children: [
+        Text(
+          '아침',
+          style: TextStyle(fontSize: 15, color: Colors.black54, fontWeight:  FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  Widget _buildUploadAfternoonFoodSubtitle() {
+    return Row(
+      children: [
+        Text(
+          '점심',
+          style: TextStyle(fontSize: 15, color: Colors.black54, fontWeight:  FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  ///식단관리 이미지 업로드 된후 생성
+  Widget _buildUploadAfteroonImages() {
+    return InkWell(
+      child: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              enableInfiniteScroll: false,
+              aspectRatio: 2.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+            items: cardList.map((card){
+              return Builder(
+                  builder:(BuildContext context){
+                    return Container(
+                      height: MediaQuery.of(context).size.height*0.30,
+                      width: MediaQuery.of(context).size.width,
+                      child: Card(
+                        color: Colors.blueAccent,
+                        child: card,
+                      ),
+                    );
+                  }
+              );
+            }).toList(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: map<Widget>(cardList, (index, url) {
+              return Container(
+                width: 500.0,
+                height: 5.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == index ? Colors.blueAccent : Colors.blueAccent,
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+      onTap: () {
+        showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            ),
+            backgroundColor: Colors.blue,
+            context: context,
+            builder: ((builder) => bottomSheet()));
+        // _pictureNumber = pictureNumber;
+      },
     );
   }
 
@@ -349,12 +601,12 @@ class _MemberDiaryState extends State<MemberDiary> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: map<Widget>(cardList, (index, url) {
               return Container(
-                width: 5.0,
+                width: 500.0,
                 height: 5.0,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
-                  color: _currentIndex == index ? Colors.blueAccent : Colors.grey,
+                  color: _currentIndex == index ? Colors.blueAccent : Colors.blueAccent,
                 ),
               );
             }),
@@ -544,7 +796,7 @@ class Item3 extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            begin: Alignment.topLeft,
+            begin: Alignment.bottomCenter,
             end: Alignment.bottomRight,
             stops: [0.3, 1],
             colors: [Color(0xff5f2c82), Color(0xff49a09d)]
